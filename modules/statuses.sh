22b4e1cd50eaee279b6759075ffbcea028e41484
lassmanjm <lassmanjm@gmail.com>
2025-02-19 14:05:49 -0500

    Move run before logic inside git_and_status_diredtory

--- modules/statuses.sh
+++ modules/statuses.sh
@@ -5,9 +5,17 @@
 # Default colors are dark grey on off-white.
 # Flags:
 #   -[g]it: If set, include git statuses (uncommitted changes, ahead behind)
+#   -[G]it: Same as above, but takes argument to set the sybmol displayed when
+#     there are uncomitted changes in the current repo. DEFAULT: 
 #   -[p]ython venv: If set, include status if python VENV is activated
-#   -[f]oreground color: The 256 color of the text font
-#   -[b]ackground color: The 256 color of the text background
+#   -[P]ython venv: Same as above, but takes argument to set the sybmol displayed when
+#     a python VENV is activated. DEFAULT: 
+#   -[f]oreground color: The 256 color of the text font. DEFAULT: dark grey.
+#   -[b]ackground color: The 256 color of the text background. DEFAULT: off-white
+#   -[d]elimiter: The delimiter string for the module. Default: "" (will be overridden
+#     by POWER_PROMPT_DELIMITER
+#   -[r]efresh rate: Time in seconds between git refreshes. I.e., until that time has passed,
+#     a git fetch will not be run to compare to upstream. DEFAULT: 30
 #
 # Usage:
 #   Default:
@@ -15,79 +23,79 @@
 #   Custom:
 #     export POWER_PROMPT_STRING="...;power_prompt_statuses -p -f 240 -b 255;..."
 
-
-# Add this to POWER_PROMPT_RUN_BEFORE function to fetch at regular intervals. This will
-# decrease latency significantly. Without adding this, fetches will not happen automatically,
-# and the ahead/behind arrows will not necessarily be accurate.
-function power_prompt_statuses_run_before(){
-  # Get current branch, return if not found meaning not in a repo directory
-  local git_branch=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
-  [[ -z $git_branch ]] && return 0
-
-  local curr_time=$(date +%s)
-  # Default to refresh every 30 seconds
-  local refresh_time=30
-  if [[ $# -gt 0 ]]; then
-    refresh_time=$1
-  fi
-
-  #Only fetch if the directory has changed or it has been long enogh
-  if [[ $(pwd) != $POWER_PROMPT_PREVIOUS_WD ]] || [[ $((curr_time-POWER_PROMPT_GIT_LAST_UPSTREAM_CHECK)) -gt $refresh_time ]]; then
-    # compare local and remote hashed (this is faster than a fetch)
-    local remote_hash=$(git ls-remote origin -h refs/heads/$git_branch 2>/dev/null| awk '{print $1}')
-    local local_hash=$(git rev-parse HEAD)
-
-    # only fetch if the hashes do not match
-    if [ "$remote_hash" != "$local_hash" ]; then
-      git fetch --quiet >/dev/null 2>&1
-    fi
-    POWER_PROMPT_GIT_LAST_UPSTREAM_CHECK=$(date +%s)
-  fi
-}
-
-
 function power_prompt_statuses(){
-  local fg=240 bg=255 delimiter text="" GIT=false PYTHON=false git_sign="" python_sign="" 
+  # Default values
+  local fg=240 bg=255 delimiter text="" GIT=false PYTHON=false git_sign="" python_sign="" refresh_rate=30
   local OPTIND
-  while getopts "b:f:d:gpG:P:" flag; do
+  while getopts "b:f:d:gG:pP:r:" flag; do
     case "${flag}" in
       f)
+	# Foreground color
 	fg=$OPTARG
 	;;
       b)
+	# Background color
 	bg=$OPTARG
 	;;
       d)
+	# Delimiter string
 	delimiter=$OPTARG
 	;;
       g)
+	# Show git statuses
 	GIT=true
 	;;
       G)
+	# Show git statuses and set unsubmitted changes symbol
 	GIT=true
 	git_sign=$OPTARG
 	;;
       p)
+	# Show python venv status
 	PYTHON=true
 	;;
       P)
+	# Show python venv status and set symbol
 	PYTHON=true
 	python_sign=$OPTARG
 	;;
+      r)
+	# Rate at which to refresh git information from upstream (run git fetch)
+	refresh_rate=$OPTARG
+	;;
     esac
-
   done
+
   shift $((OPTIND - 1))
   local statuses=""
+
+  # Check for VENV
   if [[ $PYTHON == "true" ]] && [[ -n "$VIRTUAL_ENV" ]]; then
     statuses="$python_sign "
   fi
 
+  # Check if current dir is inside a git repo
   git_branch=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
   if [[ $GIT == "true" ]] && [[ -n "$git_branch" ]]; then
+
     # check if the branch has an upstream (remote) branch
     local upstream=$(git rev-parse --abbrev-ref "@{upstream}" 2>/dev/null)
     if [ -n "$upstream" ]; then
+
+      local curr_time=$(date +%s)
+      #Only fetch if the directory has changed or it has been long enogh
+      if [[ $(pwd) != $POWER_PROMPT_PREVIOUS_WD ]] || [[ $((curr_time-POWER_PROMPT_GIT_LAST_UPSTREAM_CHECK)) -gt $refresh_rate ]]; then
+	# compare local and remote hashes (this is faster than a fetch)
+	local remote_hash=$(git ls-remote origin -h refs/heads/$git_branch 2>/dev/null| awk '{print $1}')
+	local local_hash=$(git rev-parse HEAD)
+
+	# only fetch if the hashes do not match
+	if [ "$remote_hash" != "$local_hash" ]; then
+	  git fetch --quiet >/dev/null 2>&1
+	fi
+	POWER_PROMPT_GIT_LAST_UPSTREAM_CHECK=$(date +%s)
+      fi
+
       # get the number of commits ahead/behind
       local git_ahead_behind=$(git rev-list --left-right --count "$upstream"...HEAD 2>/dev/null)
       local behind_count=$(echo "$git_ahead_behind" | awk '{print $1}')
@@ -102,11 +110,11 @@ function power_prompt_statuses(){
         statuses=" $statuses"
       fi
     fi
-    # add 󰫢 if there are uncommitted changes in git repo
+    # add git_sign if there are uncommitted changes in git repo
     if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
       statuses="$git_sign $statuses"
     fi
   fi
   statuses="$(echo "$statuses" | sed 's/[[:space:]]*$//')"
-  export POWER_PROMPT_OUTPUT="$statuses,$fg,$bg,$delimiter"
+  POWER_PROMPT_OUTPUT="$statuses,$fg,$bg,$delimiter"
 }
